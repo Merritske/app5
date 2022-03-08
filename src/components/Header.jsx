5d0528b2edcb60100b5fe8d0df2fa4fa1fdf8b1b
Merritske <merritske@gmail.com>
2022-03-08 15:26:21 +0100

    update 8

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,31 @@
-export default function Header(){
+import PropTypes from "prop-types"
+import Button from "./Button"
+export default function Header({title}){
+    function handleClick(){
+        console.log("piep")
+        }
+        
+
     return(
-        <div>
-            <h1>Hello</h1>
-        </div>
+        <header className="header">
+            <h1 >{title}</h1>
+           <Button 
+          click={handleClick}
+           color="green" 
+           text="Add"
+           />
+      
+        </header>
     )
 }
+Header.defaultProps ={
+    title: "task tracker"
+}
+Header.propTypes ={
+    title : PropTypes.string.isRequired
+}
+//css in js vb. <h1 style={headingStyle}> 
+// const headingStyle = {
+//     color: "red",
+//      backgroundColor: "black"
+// }
